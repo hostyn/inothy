62c82a81b9d43bcb11e1d9997c0137b9f9dadac7
hostyn <hostyn96@gmail.com>
2022-10-28 15:55:01 +0200

    nose

--- src/context/Providers.jsx
+++ src/context/Providers.jsx
@@ -2,7 +2,6 @@ import { AuthProvider } from "./authContext";
 import { ModalProvider } from "./modalContext";
 
 export default function Providers({ children, headers }) {
-  console.log(headers);
   return (
     <AuthProvider headers={headers}>
       <ModalProvider>{children}</ModalProvider>
