f9cfda33322d77b4e2e43498ef6ab9ecdfed5afc
hostyn <hostyn96@gmail.com>
2023-01-12 11:10:37 +0100

    Add algolia search

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,8 +1,10 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
 import styled from "styled-components";
 import { colors } from "../config/theme";
 import Img from "./Img";
 
-const InputDiv = styled.div`
+const InputDiv = styled.form`
   border: 2px solid ${(props) => colors[props.border] || colors.primary};
   border-radius: ${(props) => props.borderRadius || "999999px"};
   width: ${(props) => props.width || "auto"};
@@ -13,9 +15,13 @@ const InputDiv = styled.div`
   align-items: center;
   padding: 0 15px;
 
-  @media (max-width: 768px) {
+  ${(props) =>
+    !props.noHide &&
+    `
+    @media (max-width: 768px) {
     display: none;
   }
+  `}
 `;
 
 const StyledInput = styled.input`
@@ -26,18 +32,41 @@ const StyledInput = styled.input`
   margin: 0 10px;
   outline: none;
   background-color: transparent;
+
+  @media (max-width: 500px) {
+    ::placeholder {
+      color: white;
+    }
+  }
 `;
 
-export default function SearchBox() {
+export default function SearchBox({ ...props }) {
+  const [q, setQ] = useState("");
+  const { push } = useRouter();
+
+  const handleChange = (e) => {
+    setQ(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    push("/search?q=" + q, null, { shallow: false });
+    setQ("");
+  };
+
   return (
-    <InputDiv>
+    <InputDiv onSubmit={handleSubmit} {...props}>
       <Img
         src="/icons/search.svg"
         width="auto"
         aspectRatio="1"
         height="60%"
       ></Img>
-      <StyledInput placeholder="Universidad, carreras o asignaturas" />
+      <StyledInput
+        placeholder="Universidad, facultad, carrera o asignatura"
+        value={q}
+        onChange={handleChange}
+      />
     </InputDiv>
   );
 }
---
+++ src/pages/search.jsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import algoliaIndex from "../config/algolia";
+import SearchView from "../views/Search";
+
+export default function Search({ q }) {
+  return (
+    <>
+      <Head>
+        <title>Inothy - Buscar</title>
+      </Head>
+      <SearchView q={q} />
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { q, page } = context.query;
+
+  if (!q) return { props: {} };
+
+  const res = await algoliaIndex.search(q, { page: page || 0 });
+
+  return { props: { q: res } };
+}
