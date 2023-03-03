bcc53c3b8ce402e3334e1e0b05617e7270935410
Hostyn <ruben@martinezhostyn.com>
2023-03-03 09:40:42 +0100

    Migrate hooks to typescript

--- src/hooks/useWindowDimensions.tsx
+++ src/hooks/useWindowDimensions.tsx
@@ -1,26 +1,33 @@
 import { useState, useEffect } from 'react'
 
-function getWindowDimensions () {
+interface WindowDimensions {
+  width: number
+  height: number
+}
+
+function getWindowDimensions(): WindowDimensions {
   if (typeof window === 'undefined') return { width: 1920, height: 1080 }
   const { innerWidth: width, innerHeight: height } = window
   return {
     width,
-    height
+    height,
   }
 }
 
-export default function useWindowDimensions () {
+export default function useWindowDimensions(): WindowDimensions {
   const [windowDimensions, setWindowDimensions] = useState(
     getWindowDimensions()
   )
 
   useEffect(() => {
-    function handleResize () {
+    function handleResize(): void {
       setWindowDimensions(getWindowDimensions())
     }
 
     window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
   }, [])
 
   return windowDimensions
