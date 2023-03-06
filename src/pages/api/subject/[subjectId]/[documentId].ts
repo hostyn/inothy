9575fd5bf67f7f4c9c5036b3ea016529781f1160
Hostyn <ruben@martinezhostyn.com>
2023-03-06 20:16:31 +0100

    Migrate get document

---
+++ src/pages/api/subject/[subjectId]/[documentId].ts
@@ -0,0 +1,71 @@
+import withMethod from '@middleware/withMethod'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { firestoreAdmin } from '@config/firebaseadmin'
+
+async function getDocument(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  const { subjectId, documentId } = req.query
+
+  if (typeof subjectId !== 'string') {
+    res.status(400).json({ success: false, error: 'subject-id-required' })
+    return
+  }
+
+  if (typeof documentId !== 'string') {
+    res.status(400).json({ success: false, error: 'document-id-required' })
+    return
+  }
+
+  const subjectReference = firestoreAdmin.collection('subjects').doc(subjectId)
+
+  const documentReference = subjectReference.collection('docs').doc(documentId)
+
+  const subjectSnapshot = await subjectReference.get()
+  const documentSnapshot = await documentReference.get()
+
+  if (!documentSnapshot.exists) {
+    res.status(404).json({ success: false, error: 'not-found' })
+    return
+  }
+
+  const subjectData = subjectSnapshot.data()
+  const documentData = documentSnapshot.data()
+
+  const universitySnapshot = await firestoreAdmin
+    .collection('universities')
+    .doc(subjectData?.university)
+    .get()
+
+  const creatorSnapshot = await firestoreAdmin
+    .collection('users')
+    .doc(documentData?.createdBy)
+    .get()
+
+  const creatorData = creatorSnapshot.data()
+
+  const universityData = universitySnapshot.data()
+
+  res.status(200).json({
+    subject: { ...subjectData, id: subjectId, maxPrice: undefined },
+    university: { ...universityData, id: universitySnapshot.id },
+    id: documentId,
+    createdAt: documentData?.createdAt,
+    createdBy: creatorData?.username,
+    createdById: documentData?.createdBy,
+    description: documentData?.description,
+    file: documentData?.file,
+    fileName: documentData?.fileName,
+    name: documentData?.name,
+    price: documentData?.price,
+    rating: documentData?.rating,
+    totalRatings: documentData?.totalRatings,
+    contentType: documentData?.contentType,
+    preview: documentData?.preview,
+    sales: documentData?.sales,
+    verified: documentData?.verificationStatus === 'verified',
+  })
+}
+
+export default withMethod('GET', getDocument)
