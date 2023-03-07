04a8ed302fe01cfabd3a5b4db07d20af3a8173ba
Hostyn <ruben@martinezhostyn.com>
2023-03-07 18:57:55 +0100

    Fix constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,18 +1,18 @@
-export const ALGOLIA_APP_ID = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID ?? ''
-export const ALGOLIA_SEARCH_KEY =
-  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY ?? ''
-export const ALGOLIA_INDEX = process.env.NEXT_PUBLIC_ALGOLIA_INDEX ?? ''
-export const FIREBASE_PUBLIC = process.env.NEXT_PUBLIC_FIREBASE ?? ''
-export const FIREBASE_ADMIN_CREDENTIALS =
-  process.env.FIREBASE_ADMIN_CREDENTIALS ?? ''
+export const ALGOLIA_APP_ID = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string
+export const ALGOLIA_SEARCH_KEY = process.env
+  .NEXT_PUBLIC_ALGOLIA_SEARCH_KEY as string
+export const ALGOLIA_INDEX = process.env.NEXT_PUBLIC_ALGOLIA_INDEX as string
+export const FIREBASE_PUBLIC = process.env.NEXT_PUBLIC_FIREBASE as string
+export const FIREBASE_ADMIN_CREDENTIALS = process.env
+  .FIREBASE_ADMIN_CREDENTIALS as string
 
-export const MANGOPAY_CLIENT_ID = process.env.MANGOPAY_CLIENT_ID ?? ''
-export const MANGOPAY_API_KEY = process.env.MANGOPAY_API_KEY ?? ''
-export const MANGOPAY_ENDPOINT = process.env.MANGOPAY_ENDPOINT ?? ''
-export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL ?? ''
-export const NOREPLY_USER = process.env.NOREPLY_USER ?? ''
-export const NOREPLY_PASS = process.env.NOREPLY_PASS ?? ''
-export const MIN_PRICE = process.env.NEXT_PUBLIC_MIN_PRICE ?? ''
+export const MANGOPAY_CLIENT_ID = process.env.MANGOPAY_CLIENT_ID as string
+export const MANGOPAY_API_KEY = process.env.MANGOPAY_API_KEY as string
+export const MANGOPAY_ENDPOINT = process.env.MANGOPAY_ENDPOINT as string
+export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL as string
+export const NOREPLY_USER = process.env.NOREPLY_USER as string
+export const NOREPLY_PASS = process.env.NOREPLY_PASS as string
+export const MIN_PRICE = process.env.NEXT_PUBLIC_MIN_PRICE as string
 
 export const BadgesNames = {
   ambassador: 'Embajador',
