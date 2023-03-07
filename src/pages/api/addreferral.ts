e2917e56f072ca83b5db0efb25ef8aa88eca3716
Hostyn <ruben@martinezhostyn.com>
2023-03-07 11:49:10 +0100

    Migrate add referral

---
+++ src/pages/api/addreferral.ts
@@ -0,0 +1,36 @@
+import { firestoreAdmin } from '@config/firebaseadmin'
+import withAuthFullData from '@middleware/withAuthFullData'
+import withMethod from '@middleware/withMethod'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { ApiUser } from 'types/api'
+
+async function addReferral(
+  user: ApiUser,
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  const { ref } = JSON.parse(req.body)
+
+  if (typeof ref !== 'string') {
+    res.status(400).json({ success: false, error: 'ref-required' })
+    return
+  }
+
+  const userRefReference = firestoreAdmin.collection('referrals').doc(user.uid)
+  const userRefSnapshot = await userRefReference.get()
+
+  if (userRefSnapshot.exists) {
+    res.status(200).json({ success: false, error: 'already-referred' })
+    return
+  }
+
+  const referral: FirestoreReferral = {
+    ref,
+  }
+
+  await userRefReference.set(referral)
+
+  res.status(201).json({ success: true })
+}
+
+export default withMethod('POST', withAuthFullData(addReferral))
