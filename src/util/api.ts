e523d3a1c2ee35ee5c0cebe1dcb5c9a4816773cc
Hostyn <ruben@martinezhostyn.com>
2023-03-08 17:33:23 +0100

    Change error messages

--- src/util/api.ts
+++ src/util/api.ts
@@ -22,7 +22,7 @@ const getIdToken = (): Promise<string> | undefined =>
   auth.currentUser?.getIdToken()
 
 export async function isUsernameAvailable(username: string): Promise<boolean> {
-  if (username.length === 0) throw new Error('Username is not defined')
+  if (username.length === 0) throw new Error('username-required')
   const res = await fetch(
     `${FRONTEND_URL}/api/isusernameavailable?` +
       new URLSearchParams({
@@ -33,7 +33,7 @@ export async function isUsernameAvailable(username: string): Promise<boolean> {
     }
   )
 
-  if (res.status !== 200) throw new Error('Unexpected exception')
+  if (res.status !== 200) throw new Error('error')
 
   const { available } = await res.json()
   return available as boolean
@@ -41,7 +41,7 @@ export async function isUsernameAvailable(username: string): Promise<boolean> {
 
 export async function getUserData(): Promise<FirestoreUser> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/userdata`, {
     headers: {
@@ -54,7 +54,7 @@ export async function getUserData(): Promise<FirestoreUser> {
 
 export async function sendVerificationEmail(): Promise<void> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/sendverificationemail`, {
     headers: {
@@ -85,7 +85,7 @@ export async function getUniversities(): Promise<University[]> {
 export async function getUniversity(
   universityId: string
 ): Promise<UniversityWithSchools> {
-  if (universityId.length === 0) throw new Error('University ID Is Required')
+  if (universityId.length === 0) throw new Error('university-id-required')
 
   const response = await fetch(
     `${FRONTEND_URL}/api/universities/${universityId}`,
@@ -99,7 +99,7 @@ export async function getUniversity(
   }
 
   if (response.status === 404) {
-    throw new Error('Not found')
+    throw new Error('not-found')
   }
 
   throw new Error('error')
@@ -109,8 +109,8 @@ export async function getSchool(
   universityId: string,
   schoolId: string
 ): Promise<SchoolWithDegree> {
-  if (universityId.length === 0) throw new Error('University ID Is Required')
-  if (schoolId.length === 0) throw new Error('School ID Is Required')
+  if (universityId.length === 0) throw new Error('university-id-required')
+  if (schoolId.length === 0) throw new Error('school-id-required')
 
   const data = await fetch(
     `${FRONTEND_URL}/api/universities/${universityId}/${schoolId}`,
@@ -122,7 +122,7 @@ export async function getSchool(
   }
 
   if (data.status === 404) {
-    throw new Error('Not found')
+    throw new Error('not-found')
   }
 
   throw new Error('error')
@@ -134,9 +134,9 @@ export async function getDegree(
   degreeId: string,
   year = 1
 ): Promise<DegreeWithDocuments> {
-  if (universityId.length === 0) throw new Error('University ID Is Required')
-  if (schoolId.length === 0) throw new Error('School ID Is Required')
-  if (degreeId.length === 0) throw new Error('Degree ID Is Required')
+  if (universityId.length === 0) throw new Error('university-id-required')
+  if (schoolId.length === 0) throw new Error('school-id-required')
+  if (degreeId.length === 0) throw new Error('degree-id-required')
   const data = await fetch(
     `${FRONTEND_URL}/api/universities/${universityId}/${schoolId}/${degreeId}?` +
       new URLSearchParams({
@@ -150,7 +150,7 @@ export async function getDegree(
   }
 
   if (data.status === 404) {
-    throw new Error('Not found')
+    throw new Error('not-found')
   }
 
   throw new Error('error')
@@ -161,7 +161,7 @@ export async function getSubject(
   limit = 5,
   startAfter?: string
 ): Promise<SubjectWithDocumentsAndUniveristy> {
-  if (subjectId.length === 0) throw new Error('Subject Id is required')
+  if (subjectId.length === 0) throw new Error('subject-id-required')
 
   const data = await fetch(
     `${FRONTEND_URL}/api/subject/${subjectId}?${new URLSearchParams({
@@ -178,7 +178,7 @@ export async function getSubject(
   }
 
   if (data.status === 404) {
-    throw new Error('Not found')
+    throw new Error('not-found')
   }
 
   throw new Error('error')
@@ -188,8 +188,8 @@ export async function getDocument(
   subjectId: string,
   docId: string
 ): Promise<FullDocumentInfo> {
-  if (subjectId.length === 0) throw new Error('Subject Id is required')
-  if (docId.length === 0) throw new Error('Doc Id is required')
+  if (subjectId.length === 0) throw new Error('subject-id-required')
+  if (docId.length === 0) throw new Error('document-id-required')
 
   const data = await fetch(
     `${FRONTEND_URL}/api/subject/${subjectId}/${docId}`,
@@ -201,7 +201,7 @@ export async function getDocument(
   }
 
   if (data.status === 404) {
-    throw new Error('Not found')
+    throw new Error('not-found')
   }
 
   throw new Error('error')
@@ -214,7 +214,7 @@ export async function completeProfile(
   completeProfileData: CompleteProfileData
 ): Promise<void> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/completeprofile`, {
     headers: {
@@ -238,9 +238,8 @@ export async function completeProfile(
 
 export async function uploadFile(docData: UploadData): Promise<string> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
-  // TODO: handle errors
   const res = await fetch(`${FRONTEND_URL}/api/upload`, {
     headers: {
       authorization: `Bearer ${accessToken}`,
@@ -260,7 +259,7 @@ export async function uploadFile(docData: UploadData): Promise<string> {
 
 export async function completeKYC(completeKYCData: CompleteKYC): Promise<void> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/kyc`, {
     headers: {
@@ -282,7 +281,7 @@ export async function completeKYC(completeKYCData: CompleteKYC): Promise<void> {
 
 export async function createCardRegistration(): Promise<CreateCardRegistration> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/createcardregistration`, {
     headers: {
@@ -302,7 +301,7 @@ export async function completeCardRegistration(
 ): Promise<void> {
   if (id.length === 0) throw new Error('id-required')
   if (registrationData.length === 0)
-    throw new Error('registrationData-required')
+    throw new Error('registration-data-required')
 
   const res = await fetch(`${FRONTEND_URL}/api/completecardregistration`, {
     method: 'POST',
@@ -317,7 +316,7 @@ export async function completeCardRegistration(
 
 export async function getCards(): Promise<card.CardData[]> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/getcards`, {
     method: 'GET',
@@ -335,7 +334,7 @@ export async function getCards(): Promise<card.CardData[]> {
 
 export async function buy(data: BuyParams): Promise<BuyResponse> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/buy`, {
     method: 'POST',
@@ -365,7 +364,7 @@ export async function getTransaction(
   transactionId: string
 ): Promise<FirestoreTransaction> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/transaction/${transactionId}`, {
     method: 'GET',
@@ -385,7 +384,7 @@ export async function getDownloadUrl(
   documentId: string
 ): Promise<string> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(
     `${FRONTEND_URL}/api/getdownloadurl/${subjectId}/${documentId}`,
@@ -405,7 +404,7 @@ export async function getDownloadUrl(
 }
 
 export async function getUser(userId: string): Promise<GetUserResponse> {
-  if (userId.length === 0) throw new Error('UserId is required')
+  if (userId.length === 0) throw new Error('user-id-required')
 
   const res = await fetch(`${FRONTEND_URL}/api/user/${userId}`, {
     method: 'GET',
@@ -417,7 +416,7 @@ export async function getUser(userId: string): Promise<GetUserResponse> {
 
 export async function getBalance(): Promise<number> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/getbalance`, {
     method: 'GET',
@@ -434,10 +433,10 @@ export async function getBalance(): Promise<number> {
 export async function updateBankAccount(
   iban: string
 ): Promise<bankAccount.IBANData> {
-  if (iban.length === 0) throw new Error('Iban is required')
+  if (iban.length === 0) throw new Error('iban-required')
 
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/updatebankaccount`, {
     method: 'POST',
@@ -454,13 +453,13 @@ export async function updateBankAccount(
     return await res.json()
   }
 
-  if (res.status === 400) throw new Error('Bad Request')
-  throw new Error('Internal server errror')
+  if (res.status === 400) throw new Error('error')
+  throw new Error('error')
 }
 
 export async function getBankAccount(): Promise<bankAccount.Data | null> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/getbankaccount`, {
     method: 'GET',
@@ -482,7 +481,7 @@ export async function getBankAccount(): Promise<bankAccount.Data | null> {
 
 export async function payout(): Promise<void> {
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/payout`, {
     method: 'POST',
@@ -505,7 +504,7 @@ export async function deleteCard(cardId: string): Promise<void> {
   if (cardId.length === 0) throw new Error('CardId is required')
 
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/deletecard`, {
     method: 'POST',
@@ -516,7 +515,7 @@ export async function deleteCard(cardId: string): Promise<void> {
   })
 
   if (res.status === 200) return
-  throw new Error('internal-server-error')
+  throw new Error('error')
 }
 
 export async function sendResetPasswordEmail(email: string): Promise<void> {
@@ -530,10 +529,10 @@ export async function sendResetPasswordEmail(email: string): Promise<void> {
 }
 
 export async function addReferral(ref: string): Promise<void> {
-  if (ref.length === 0) throw new Error('Ref is required')
+  if (ref.length === 0) throw new Error('ref-required')
 
   const accessToken = await getIdToken()
-  if (accessToken == null) throw new Error('Unauthenticated')
+  if (accessToken == null) throw new Error('unauthenticated')
 
   const res = await fetch(`${FRONTEND_URL}/api/addreferral`, {
     method: 'POST',
