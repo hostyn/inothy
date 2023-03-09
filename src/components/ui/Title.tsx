1920fd73adfac25449c3e857f69d253379a34657
Hostyn <ruben@martinezhostyn.com>
2023-03-09 19:24:50 +0100

    Create Title ui component

---
+++ src/components/ui/Title.tsx
@@ -0,0 +1,40 @@
+import { colors } from '@config/theme'
+import styled from 'styled-components'
+
+interface TitleProps {
+  fontFamily?: string
+  fontSize?: string
+  color?: keyof typeof colors
+  margin?: string
+  width?: string
+  display?: string
+  alignItems?: string
+  fontWeight?: string
+  textAlign?: string
+  userSelect?: string
+  minWidth?: string
+  lineHeight?: string
+  maxWidth?: string
+  cursor?: string
+  lineBreak?: string
+}
+
+const Title = styled.p<TitleProps>`
+  display: ${props => props.display ?? 'initial'};
+  align-items: ${props => props.alignItems ?? 'inital'};
+  font-family: ${props => props.fontFamily ?? 'VarelaRound'};
+  font-size: ${props => props.fontSize ?? '2rem'};
+  font-weight: ${props => props.fontWeight ?? 'normal'};
+  color: ${props => colors[props.color ?? 'primary']};
+  text-align: ${props => props.textAlign ?? 'left'};
+  margin: ${props => props.margin ?? '0'};
+  user-select: ${props => props.userSelect ?? 'initial'};
+  width: ${props => props.width ?? 'initial'};
+  min-width: ${props => props.minWidth ?? 'initial'};
+  line-height: ${props => props.lineHeight ?? 'initial'};
+  max-width: ${props => props.maxWidth ?? 'initial'};
+  cursor: ${props => props.cursor ?? 'inherit'};
+  line-break: ${props => props.lineBreak ?? 'auto'};
+`
+
+export default Title
