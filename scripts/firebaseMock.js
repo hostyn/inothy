331f50fe3f8c0f9dcd6ee53d86adc2326fb8104d
Hostyn <ruben@martinezhostyn.com>
2023-05-18 11:14:30 +0200

    fix: :bug: Remove comments

--- scripts/firebaseMock.js
+++ scripts/firebaseMock.js
@@ -40,95 +40,95 @@ async function main() {
   console.log('[#] Deleting all documents')
   await firestoreAdmin.recursiveDelete(firestoreAdmin.collection('users'))
 
-  // // Delete all referrals
-  // console.log('[#] Deleting all referrals')
-  // await firestoreAdmin.recursiveDelete(firestoreAdmin.collection('referrals'))
+  // Delete all referrals
+  console.log('[#] Deleting all referrals')
+  await firestoreAdmin.recursiveDelete(firestoreAdmin.collection('referrals'))
 
-  // // Delete all transactions
-  // console.log('[#] Deleting all transactions')
-  // await firestoreAdmin.recursiveDelete(
-  //   firestoreAdmin.collection('transactions')
-  // )
+  // Delete all transactions
+  console.log('[#] Deleting all transactions')
+  await firestoreAdmin.recursiveDelete(
+    firestoreAdmin.collection('transactions')
+  )
 
-  // // Delete all universities
-  // console.log('[#] Deleting all universities')
-  // await firestoreAdmin.recursiveDelete(
-  //   firestoreAdmin.collection('universities')
-  // )
+  // Delete all universities
+  console.log('[#] Deleting all universities')
+  await firestoreAdmin.recursiveDelete(
+    firestoreAdmin.collection('universities')
+  )
 
-  // // Delete all subjects
-  // console.log('[#] Deleting all subjects')
-  // await firestoreAdmin.recursiveDelete(firestoreAdmin.collection('subjects'))
+  // Delete all subjects
+  console.log('[#] Deleting all subjects')
+  await firestoreAdmin.recursiveDelete(firestoreAdmin.collection('subjects'))
 
-  // // Delete all files
-  // console.log('[#] Deleting all files')
-  // await storageAdmin.deleteFiles({ prefix: 'files' })
+  // Delete all files
+  console.log('[#] Deleting all files')
+  await storageAdmin.deleteFiles({ prefix: 'files' })
 
-  // // Delete all previews
-  // console.log('[#] Deleting all previews')
-  // await storageAdmin.deleteFiles({ prefix: 'previews' })
+  // Delete all previews
+  console.log('[#] Deleting all previews')
+  await storageAdmin.deleteFiles({ prefix: 'previews' })
 
-  // // Delete all university_logos
-  // console.log('[#] Deleting all university logos')
-  // await storageAdmin.deleteFiles({ prefix: 'university_logos' })
+  // Delete all university_logos
+  console.log('[#] Deleting all university logos')
+  await storageAdmin.deleteFiles({ prefix: 'university_logos' })
 
-  // // Write new database
-  // console.log('[#] Creating new database')
-  // const universities = JSON.parse(
-  //   fs.readFileSync('scripts/resources/universities.json')
-  // )
-  // const subjects = JSON.parse(
-  //   fs.readFileSync('scripts/resources/subjects.json')
-  // )
+  // Write new database
+  console.log('[#] Creating new database')
+  const universities = JSON.parse(
+    fs.readFileSync('scripts/resources/universities.json')
+  )
+  const subjects = JSON.parse(
+    fs.readFileSync('scripts/resources/subjects.json')
+  )
 
-  // console.log('[#] Creating universities')
-  // const bulk = firestoreAdmin.bulkWriter()
-  // universities.forEach(univeristy => {
-  //   const logoBuffer = fs.readFileSync(
-  //     `scripts/resources/${univeristy.symbol}.png`
-  //   )
+  console.log('[#] Creating universities')
+  const bulk = firestoreAdmin.bulkWriter()
+  universities.forEach(univeristy => {
+    const logoBuffer = fs.readFileSync(
+      `scripts/resources/${univeristy.symbol}.png`
+    )
 
-  //   const universityLogoRef = storageAdmin.file(
-  //     `univeristy_logo/${univeristy.uid}.png`
-  //   )
+    const universityLogoRef = storageAdmin.file(
+      `univeristy_logo/${univeristy.uid}.png`
+    )
 
-  //   const logoUrl = universityLogoRef.publicUrl()
+    const logoUrl = universityLogoRef.publicUrl()
 
-  //   universityLogoRef
-  //     .save(logoBuffer)
-  //     .then(() => universityLogoRef.makePublic())
+    universityLogoRef
+      .save(logoBuffer)
+      .then(() => universityLogoRef.makePublic())
 
-  //   const { uid: univeristyUid, schools, ...universityData } = univeristy
+    const { uid: univeristyUid, schools, ...universityData } = univeristy
 
-  //   const universityRef = firestoreAdmin
-  //     .collection('universities')
-  //     .doc(univeristyUid)
+    const universityRef = firestoreAdmin
+      .collection('universities')
+      .doc(univeristyUid)
 
-  //   bulk.create(universityRef, {
-  //     ...universityData,
-  //     logoUrl,
-  //   })
+    bulk.create(universityRef, {
+      ...universityData,
+      logoUrl,
+    })
 
-  //   schools.forEach(school => {
-  //     const { uid: schoolUid, degrees, ...schoolData } = school
-  //     const schoolRef = universityRef.collection('schools').doc(schoolUid)
-  //     bulk.create(schoolRef, schoolData)
+    schools.forEach(school => {
+      const { uid: schoolUid, degrees, ...schoolData } = school
+      const schoolRef = universityRef.collection('schools').doc(schoolUid)
+      bulk.create(schoolRef, schoolData)
 
-  //     degrees.forEach(degree => {
-  //       const { uid: degreeUid, ...degreeData } = degree
-  //       const degreeRef = schoolRef.collection('degrees').doc(degreeUid)
-  //       bulk.create(degreeRef, degreeData)
-  //     })
-  //   })
-  // })
+      degrees.forEach(degree => {
+        const { uid: degreeUid, ...degreeData } = degree
+        const degreeRef = schoolRef.collection('degrees').doc(degreeUid)
+        bulk.create(degreeRef, degreeData)
+      })
+    })
+  })
 
-  // console.log('[#] Creating subjects')
-  // subjects.forEach(subject => {
-  //   const { uid, ...subjectData } = subject
-  //   bulk.create(firestoreAdmin.collection('subjects').doc(uid), subjectData)
-  // })
+  console.log('[#] Creating subjects')
+  subjects.forEach(subject => {
+    const { uid, ...subjectData } = subject
+    bulk.create(firestoreAdmin.collection('subjects').doc(uid), subjectData)
+  })
 
-  // await bulk.flush()
+  await bulk.flush()
 
   // Creating users
   console.log('[#] Creating user')
