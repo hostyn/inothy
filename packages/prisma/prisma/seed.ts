1527ce6af96c37ca263202d6ae03c281a242c62e
Hostyn <ruben@martinezhostyn.com>
2023-06-26 23:39:53 +0200

    feat(database): :sparkles: Add universities mock to seed

--- packages/prisma/prisma/seed.ts
+++ packages/prisma/prisma/seed.ts
@@ -1,8 +1,46 @@
 import { PrismaClient } from '@prisma/client'
+import { authAdmin, storageAdmin } from 'firebase-admin-config'
+import universities from '../resources/universities.json'
+import subjects from '../resources/subjects.json'
+import schools from '../resources/schools.json'
+import degrees from '../resources/degrees.json'
+import subjectsWithYear from '../resources/subjectsWithYear.json'
+import fs from 'fs'
 
 const prisma = new PrismaClient()
 
-async function main() {}
+async function main() {
+  await Promise.all(
+    universities.map(async university => {
+      const logoBuffer = fs.readFileSync(`resources/${university.symbol}.png`)
+
+      const universityLogoRef = storageAdmin.file(
+        `university_logo/${university.id}.png`
+      )
+
+      const logoUrl = universityLogoRef.publicUrl()
+
+      universityLogoRef
+        .save(logoBuffer)
+        .then(() => universityLogoRef.makePublic())
+
+      await prisma.university.create({
+        data: {
+          id: university.id,
+          name: university.name,
+          symbol: university.symbol,
+          logoUrl: logoUrl,
+          url: university.url,
+        },
+      })
+    })
+  )
+
+  await prisma.school.createMany({ data: schools })
+  await prisma.degree.createMany({ data: degrees })
+  await prisma.subjectWithYear.createMany({ data: subjectsWithYear })
+  await prisma.subject.createMany({ data: subjects })
+}
 
 main()
   .then(async () => {
