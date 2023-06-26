0f1eccbf0b45b86b91d15e4740e9fbbfb10952b7
Hostyn <ruben@martinezhostyn.com>
2023-06-26 23:39:10 +0200

    feat(repo): :sparkles: Add firebase-admin-config package

---
+++ packages/firebase-admin-config/index.ts
@@ -0,0 +1,19 @@
+import admin from 'firebase-admin'
+
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS as string)
+    ),
+  })
+} catch (error) {
+  console.error('Firebase admin initialization error', error)
+}
+
+export const authAdmin = admin.auth()
+export const firestoreAdmin = admin.firestore()
+export const storageAdmin = admin
+  .storage()
+  .bucket(JSON.parse(process.env.NEXT_PUBLIC_FIREBASE as string).storageBucket)
+
+export default admin
