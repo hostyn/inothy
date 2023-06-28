c929c64d909d0a8527b76fb6670d3274a85ad553
Hostyn <ruben@martinezhostyn.com>
2023-06-28 09:33:46 +0200

    feat(backend): :sparkles: Add getUserData endpoint

--- apps/backend/src/routes/auth.ts
+++ apps/backend/src/routes/auth.ts
@@ -2,6 +2,26 @@ import { publicProcedure } from '../procedures'
 import { createTRPCRouter } from '../trpc'
 
 export const authRouter = createTRPCRouter({
+  getUserData: publicProcedure.query(async ({ ctx }) => {
+    if (ctx.user.id == null) {
+      return {}
+    }
+
+    const userData = await ctx.prisma.user.findUnique({
+      where: { uid: ctx.user.id },
+      select: {
+        id: true,
+        username: true,
+        avatarUrl: true,
+        biography: true,
+        degreeId: true,
+        email: true,
+        uid: true,
+      },
+    })
+    return userData
+  }),
+
   test: publicProcedure.query(async ({ ctx }) => {
     return { message: 'Hello world!' }
   }),
