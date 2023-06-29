64e547c1735275ac93b6a36fde95d52ae930de41
Hostyn <ruben@martinezhostyn.com>
2023-06-29 16:26:42 +0200

    feat(frontend): :sparkles: Add universities page

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,21 +1,13 @@
 import Head from 'next/head'
-import { trpc } from '@services/trpc'
 import publicContentSSR from '@middleware/publicContentSSR'
 import publicContent from '@middleware/publicContent'
 import { type NextPage } from 'next'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '@config/firebase'
 import { useAuthUser } from 'next-firebase-auth'
-import { css } from '@styled-system/css'
-import { Button } from '@ui/Button'
-import { AiOutlineSearch } from 'react-icons/ai'
 import App from '@components/App'
 
 const Home: NextPage = () => {
-  const userData = trpc.auth.getUserData.useQuery()
-
-  console.log(userData.data)
-
   const user = useAuthUser()
 
   const handleLogin = async (): Promise<void> => {
--- apps/frontend/src/pages/universities.tsx
+++ apps/frontend/src/pages/universities.tsx
@@ -1,30 +1,105 @@
-import { type GetServerSideProps } from 'next'
 import Head from 'next/head'
-import type { University } from 'types/api'
-import { getUniversities } from '@util/api'
-import UniversitiesView from '@views/Universities'
+import publicContentSSR from '@middleware/publicContentSSR'
+import publicContent from '@middleware/publicContent'
+import { trpc } from '@services/trpc'
+import App from '@components/App'
+import { css } from '@styled-system/css'
+import { LiaUniversitySolid } from 'react-icons/lia'
+import SearchBar from '@ui/SearchBar'
+import Image from 'next/image'
 
-interface UniversitiesProps {
-  universities: University[]
-}
+function Universities(): JSX.Element {
+  const { data: universities } = trpc.universities.getUniversities.useQuery()
 
-export default function Universities({
-  universities,
-}: UniversitiesProps): JSX.Element {
   return (
     <>
       <Head>
         <title>Inothy - Universidades</title>
         <meta name="robots" content="index,follow" />
       </Head>
-      <UniversitiesView universities={universities} />
+      <App>
+        <div className={css({ width: '5xl', margin: 'auto' })}>
+          <div
+            className={css({
+              py: 'md',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            })}
+          >
+            <div
+              className={css({
+                display: 'flex',
+                alignItems: 'center',
+                gap: 'sm',
+              })}
+            >
+              <LiaUniversitySolid
+                size={24}
+                className={css({ fill: 'primary.500' })}
+              />
+              <h1 className={css({ fontSize: 'xl', color: 'primary.500' })}>
+                Universidades
+              </h1>
+            </div>
+
+            <SearchBar />
+          </div>
+
+          <div className={css({ height: '2px', bg: 'grey.100' })} />
+
+          <div
+            className={css({
+              display: 'flex',
+              flexDirection: 'column',
+              py: 'md',
+              gap: 'xs',
+            })}
+          >
+            {universities?.map(university => (
+              <div
+                key={university.id}
+                className={css({
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                })}
+              >
+                <div
+                  className={css({
+                    display: 'flex',
+                    gap: 'sm',
+                    alignItems: 'center',
+                  })}
+                >
+                  <Image
+                    src={university.logoUrl}
+                    alt={`Logo de la ${university.name}`}
+                    width={32}
+                    height={32}
+                    className={css({ borderRadius: 'mf' })}
+                  />
+                  <p
+                    className={css({
+                      fontSize: 'lg',
+                      fontWeight: '600',
+                      color: 'primary.500',
+                    })}
+                  >
+                    {university.name}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </App>
     </>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async context => {
-  const universities = await getUniversities()
-  return {
-    props: { universities },
-  }
-}
+export default publicContent(Universities)
+export const getServerSideProps = publicContentSSR(async ({ helper }) => {
+  await helper.universities.getUniversities.prefetch()
+  return { props: {} }
+})
