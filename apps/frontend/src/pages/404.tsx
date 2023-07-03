1a94180033b57bcd39d26ce9ec7cfaeb804136ad
Hostyn <ruben@martinezhostyn.com>
2023-07-03 19:38:40 +0200

    feat(frontend): :sparkles: Add 404 page

--- apps/frontend/src/pages/404.tsx
+++ apps/frontend/src/pages/404.tsx
@@ -1,17 +1,7 @@
-import styled from 'styled-components'
 import App from '@components/App'
-import Link from 'next/link'
+import { css } from '@styled-system/css'
+import { LinkButton } from '@ui/Button'
 import Head from 'next/head'
-import { A, Img, Text } from '@ui'
-
-const NotFoundDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: inherit;
-  min-width: 100vw;
-`
 
 export default function notFound(): JSX.Element {
   return (
@@ -19,7 +9,50 @@ export default function notFound(): JSX.Element {
       <Head>
         <title>Inothy - 404 No encontrado</title>
       </Head>
-      <h1>404</h1>
+      <App>
+        <div
+          className={css({
+            width: '5xl',
+            margin: 'auto',
+            display: 'flex',
+            flexDir: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: 'inherit',
+            gap: 'md',
+          })}
+        >
+          <div
+            className={css({
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            })}
+          >
+            <h1
+              className={css({
+                fontSize: '4xl',
+                fontWeight: 'bold',
+                color: 'primary.500',
+                lineHeight: '1',
+              })}
+            >
+              404
+            </h1>
+            <p
+              className={css({
+                color: 'primary.500',
+                fontWeight: '500',
+                fontSize: 'lg',
+              })}
+            >
+              Parece que esta página no existe.
+            </p>
+          </div>
+          <LinkButton href="/">Volver al inicio</LinkButton>
+        </div>
+      </App>
     </>
   )
 }
