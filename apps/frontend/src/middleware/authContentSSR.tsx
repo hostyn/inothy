bf6d9793c32e1538b675bd1b14801cdc24e8ebcb
Hostyn <ruben@martinezhostyn.com>
2023-07-16 10:41:08 +0200

    feat(frontend): :sparkles: Add authContentSSR middleware

---
+++ apps/frontend/src/middleware/authContentSSR.tsx
@@ -0,0 +1,51 @@
+import { useServerSideHelper } from '@hooks/useServerSideHelper'
+import type {
+  GetServerSidePropsResult,
+  GetServerSideProps,
+  GetServerSidePropsContext,
+} from 'next'
+import {
+  type AuthUser,
+  withAuthUserTokenSSR,
+  AuthAction,
+} from 'next-firebase-auth'
+
+const authContentSSR = <Type,>(
+  getServerSideProps?: (
+    ctx: GetServerSidePropsContext & {
+      AuthUser: AuthUser
+      helper: ReturnType<typeof useServerSideHelper>
+    }
+  ) => Promise<GetServerSidePropsResult<Type>>
+): GetServerSideProps => {
+  return withAuthUserTokenSSR({
+    whenAuthed: AuthAction.REDIRECT_TO_APP,
+  })(async ctx => {
+    const helper = useServerSideHelper(ctx.AuthUser)
+
+    await helper.auth.getUserData.prefetch()
+
+    if (getServerSideProps == null) {
+      return {
+        props: {
+          trpcState: helper.dehydrate(),
+        },
+      }
+    }
+
+    const res = await getServerSideProps({ ...ctx, helper })
+
+    if ('props' in res) {
+      return {
+        props: {
+          ...res.props,
+          trpcState: helper.dehydrate(),
+        },
+      }
+    }
+
+    return res
+  })
+}
+
+export default authContentSSR
