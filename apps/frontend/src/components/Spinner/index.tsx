85512fc9660b8c44202a91d75c17e73a3d6bd0e4
Hostyn <ruben@martinezhostyn.com>
2023-07-16 18:47:03 +0200

    feat(frontend): :sparkles: Add Spinner component

---
+++ apps/frontend/src/components/Spinner/index.tsx
@@ -0,0 +1,18 @@
+import { cx } from '@styled-system/css'
+import spinerClass from './Spinner.module.css'
+
+export default function Spinner({
+  className,
+}: {
+  className?: string
+}): JSX.Element {
+  return (
+    <svg
+      className={cx(spinerClass.spinner, className)}
+      viewBox="25 25 50 50"
+      strokeWidth="5"
+    >
+      <circle cx="50" cy="50" r="20" />
+    </svg>
+  )
+}
