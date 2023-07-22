75f25b1cdfebef1ad3866449cdb8e1adcdda7c89
Hostyn <ruben@martinezhostyn.com>
2023-07-22 12:48:54 +0200

    feat(frontend): :lipstick: Add PageLayout ui component

---
+++ apps/frontend/src/components/ui/PageLayout.tsx
@@ -0,0 +1,54 @@
+import { PageSpacing } from './PageSpacing'
+import SearchBar from './SearchBar'
+import { Separator } from './Separator'
+import { css } from '@styled-system/css'
+import { type IconType } from 'react-icons'
+
+interface PageLayoutProps {
+  children: React.ReactNode
+  title: string
+  Icon: IconType
+  searchBar?: boolean
+}
+
+export default function PageLayout({
+  children,
+  title,
+  Icon,
+  searchBar = false,
+}: PageLayoutProps): JSX.Element {
+  return (
+    <PageSpacing>
+      <div
+        className={css({
+          py: 'md',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        })}
+      >
+        <div
+          className={css({
+            display: 'flex',
+            alignItems: 'center',
+            gap: 'sm',
+          })}
+        >
+          <Icon size={24} className={css({ color: 'primary.500' })} />
+          <h1 className={css({ fontSize: 'xl', color: 'primary.500' })}>
+            {title}
+          </h1>
+        </div>
+        {searchBar && <SearchBar />}
+      </div>
+
+      <Separator
+        className={css({
+          mb: 'md',
+        })}
+      />
+
+      {children}
+    </PageSpacing>
+  )
+}
