3b5fa7b301d6af782cfa349f6d209de7e972acac
Hostyn <ruben@martinezhostyn.com>
2023-07-22 16:58:32 +0200

    feat(frontend): :recycle: Abstract withAuthuserSSR

--- apps/frontend/src/middleware/authContentSSR.tsx
+++ apps/frontend/src/middleware/authContentSSR.tsx
@@ -1,51 +1,8 @@
-import { useServerSideHelper } from '@hooks/useServerSideHelper'
-import type {
-  GetServerSidePropsResult,
-  GetServerSideProps,
-  GetServerSidePropsContext,
-} from 'next'
-import {
-  type AuthUser,
-  withAuthUserTokenSSR,
-  AuthAction,
-} from 'next-firebase-auth'
+import { AuthAction } from 'next-firebase-auth'
+import withAuthUserSSR from './withAuthUserSSR'
 
-const authContentSSR = <Type,>(
-  getServerSideProps?: (
-    ctx: GetServerSidePropsContext & {
-      AuthUser: AuthUser
-      helper: ReturnType<typeof useServerSideHelper>
-    }
-  ) => Promise<GetServerSidePropsResult<Type>>
-): GetServerSideProps => {
-  return withAuthUserTokenSSR({
-    whenAuthed: AuthAction.REDIRECT_TO_APP,
-  })(async ctx => {
-    const helper = useServerSideHelper(ctx.AuthUser)
-
-    await helper.auth.getUserData.prefetch()
-
-    if (getServerSideProps == null) {
-      return {
-        props: {
-          trpcState: helper.dehydrate(),
-        },
-      }
-    }
-
-    const res = await getServerSideProps({ ...ctx, helper })
-
-    if ('props' in res) {
-      return {
-        props: {
-          ...res.props,
-          trpcState: helper.dehydrate(),
-        },
-      }
-    }
-
-    return res
-  })
-}
+const authContentSSR = withAuthUserSSR({
+  whenAuthed: AuthAction.REDIRECT_TO_APP,
+})
 
 export default authContentSSR
--- apps/frontend/src/middleware/protectedContentSSR.tsx
+++ apps/frontend/src/middleware/protectedContentSSR.tsx
@@ -1,51 +1,8 @@
-import { useServerSideHelper } from '@hooks/useServerSideHelper'
-import type {
-  GetServerSidePropsResult,
-  GetServerSideProps,
-  GetServerSidePropsContext,
-} from 'next'
-import {
-  type AuthUser,
-  withAuthUserTokenSSR,
-  AuthAction,
-} from 'next-firebase-auth'
+import { AuthAction } from 'next-firebase-auth'
+import withAuthUserSSR from './withAuthUserSSR'
 
-const protectedContentSSR = <Type,>(
-  getServerSideProps?: (
-    ctx: GetServerSidePropsContext & {
-      AuthUser: AuthUser
-      helper: ReturnType<typeof useServerSideHelper>
-    }
-  ) => Promise<GetServerSidePropsResult<Type>>
-): GetServerSideProps => {
-  return withAuthUserTokenSSR({
-    whenUnauthed: AuthAction.REDIRECT_TO_LOGIN,
-  })(async ctx => {
-    const helper = useServerSideHelper(ctx.AuthUser)
-
-    await helper.auth.getUserData.prefetch()
-
-    if (getServerSideProps == null) {
-      return {
-        props: {
-          trpcState: helper.dehydrate(),
-        },
-      }
-    }
-
-    const res = await getServerSideProps({ ...ctx, helper })
-
-    if ('props' in res) {
-      return {
-        props: {
-          ...res.props,
-          trpcState: helper.dehydrate(),
-        },
-      }
-    }
-
-    return res
-  })
-}
+const protectedContentSSR = withAuthUserSSR({
+  whenUnauthed: AuthAction.REDIRECT_TO_LOGIN,
+})
 
 export default protectedContentSSR
