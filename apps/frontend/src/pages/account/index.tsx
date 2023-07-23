b47b1be86807065784cb8191978c3c1f19f3f33e
Hostyn <ruben@martinezhostyn.com>
2023-07-23 09:30:18 +0200

    fix(frontend): :bug: Fix /account redirect, add protectedContent

--- apps/frontend/src/pages/account/index.tsx
+++ apps/frontend/src/pages/account/index.tsx
@@ -1,9 +1,9 @@
-import { type GetServerSideProps } from 'next'
+import protectedContentSSR from '@middleware/protectedContentSSR'
 
 export default function Account(): JSX.Element {
   return <></>
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  return { redirect: { permanent: true, destination: '/account/general' } }
-}
+export const getServerSideProps = protectedContentSSR(async () => ({
+  redirect: { permanent: true, destination: '/account/general' },
+}))
