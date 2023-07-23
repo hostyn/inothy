ea11760c2388e286eb5728e6c7b5c2a499bc2f2e
Hostyn <ruben@martinezhostyn.com>
2023-07-23 12:16:56 +0200

    feat(backend): :sparkles: Add auth.changeUsername endpoint

--- apps/backend/src/routes/auth.ts
+++ apps/backend/src/routes/auth.ts
@@ -1,5 +1,7 @@
-import { publicProcedure } from '../procedures'
+import { z } from 'zod'
+import { protectedProcedure, publicProcedure } from '../procedures'
 import { createTRPCRouter } from '../trpc'
+import { TRPCError } from '@trpc/server'
 
 export const authRouter = createTRPCRouter({
   getUserData: publicProcedure.query(async ({ ctx }) => {
@@ -22,4 +24,48 @@ export const authRouter = createTRPCRouter({
 
     return userData
   }),
+
+  changeUsername: protectedProcedure
+    .input(
+      z.object({
+        username: z
+          .string()
+          .min(3, 'username-too-short')
+          .max(30, 'username-too-long')
+          .regex(
+            /^[a-z_\d](?!.*?\.{2})[a-z._\d]{1,28}[a-z_\d]$/,
+            'username-invalid'
+          ),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const user = await ctx.prisma.user.findUnique({
+        where: { uid: ctx.user.id ?? '' },
+      })
+
+      if (user?.username === input.username) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'already-username',
+        })
+      }
+
+      const usernameExists = await ctx.prisma.user.findFirst({
+        where: { username: input.username },
+      })
+
+      if (usernameExists != null) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'username-already-exists',
+        })
+      }
+
+      await ctx.prisma.user.update({
+        where: { uid: ctx.user.id ?? '' },
+        data: { username: input.username },
+      })
+
+      return { success: true }
+    }),
 })
