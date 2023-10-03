315b22513648a363ba4b2fdf0e6f8dbe303a5d11
Hostyn <ruben@martinezhostyn.com>
2023-10-03 10:00:58 +0100

    fix(frontend): :rotating_light: Fix linter and linter warnings

--- apps/frontend/.eslintrc.js
+++ apps/frontend/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['custom'],
+  extends: ['custom', 'plugin:@next/next/recommended'],
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
