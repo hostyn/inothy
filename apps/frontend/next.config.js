981734ed312c8c372ede6749df5ad14a9c9e6df4
Hostyn <ruben@martinezhostyn.com>
2023-10-03 11:04:12 +0100

    build(repo): :green_heart: Add prisma monorepo plugin

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,14 +1,27 @@
 // next.config.js
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
   reactStrictMode: true,
   transpilePackages: ['backend'],
   images: {
-    domains: ['storage.googleapis.com', 'i0.wp.com', 'lh3.googleusercontent.com']
+    domains: [
+      'storage.googleapis.com',
+      'i0.wp.com',
+      'lh3.googleusercontent.com',
+    ],
   },
   i18n: {
     locales: ['es'],
-    defaultLocale: 'es'
-  }
+    defaultLocale: 'es',
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
 }
