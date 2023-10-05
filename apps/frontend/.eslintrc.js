4a533cd26203b347638f483725b6170e45d3d6b3
Hostyn <ruben@martinezhostyn.com>
2023-10-05 16:59:32 +0100

    feat(frontend): :rotating_light: Add web-vitals eslint config and fix lint errors.

--- apps/frontend/.eslintrc.js
+++ apps/frontend/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['custom', 'plugin:@next/next/recommended'],
+  extends: ['custom', 'plugin:@next/next/recommended', 'next/core-web-vitals'],
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
--- apps/frontend/src/views/Upload/components/DropZone.tsx
+++ apps/frontend/src/views/Upload/components/DropZone.tsx
@@ -62,7 +62,7 @@ export default function DropZone({
 
   useEffect(() => {
     onFile(file)
-  }, [file])
+  }, [file, onFile])
 
   return (
     <>
