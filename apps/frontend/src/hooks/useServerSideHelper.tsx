0a5bffe14640f4421a2eb76a15a15197ae29c9f4
Hostyn <ruben@martinezhostyn.com>
2023-10-25 09:40:37 +0100

    feat(repo): :sparkles: Add NextApiRequest to tRPC context

--- apps/frontend/src/hooks/useServerSideHelper.tsx
+++ apps/frontend/src/hooks/useServerSideHelper.tsx
@@ -2,10 +2,14 @@ import { appRouter, createInnerTRPCContext } from 'backend'
 import { createServerSideHelpers } from '@trpc/react-query/server'
 import SuperJSON from 'superjson'
 import { type AuthUser } from 'next-firebase-auth'
+import type { NextApiRequest } from 'next'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const useServerSideHelper = (authUser: AuthUser) => {
-  const context = createInnerTRPCContext({ user: authUser })
+export const useServerSideHelper = (
+  req: NextApiRequest,
+  authUser: AuthUser
+) => {
+  const context = createInnerTRPCContext({ req, user: authUser })
 
   const helper = createServerSideHelpers({
     router: appRouter,
