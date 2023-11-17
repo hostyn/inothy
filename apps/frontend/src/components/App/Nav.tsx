fe69907094c4c4ab8329fc301f3d60593dfaeece
Hostyn <ruben@martinezhostyn.com>
2023-11-17 11:43:08 +0000

    fix(frontend): :bug: Refactor navbar dropdown and fix bug in open animation

--- apps/frontend/src/components/App/Nav.tsx
+++ apps/frontend/src/components/App/Nav.tsx
@@ -2,7 +2,7 @@ import Imagotipo from '@components/Imagotipo'
 import useAuth from '@hooks/useAuth'
 import { css } from '@styled-system/css'
 import { ButtonLink } from '@ui/Button'
-import { Link, LinkButton } from '@ui/Link'
+import { Link } from '@ui/Link'
 import SearchBar from '@ui/SearchBar'
 import Image from 'next/image'
 import {
@@ -13,32 +13,11 @@ import {
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import { Separator } from '@ui/Separator'
 import { HiOutlineLibrary } from 'react-icons/hi'
-import { MdLockOutline } from 'react-icons/md'
+import { MdLockOutline, MdOutlineLogout } from 'react-icons/md'
 import { IoCardOutline, IoWalletOutline } from 'react-icons/io5'
+import DropdownLink from './components/DropdownLink'
 
 const divStyles = css({ display: 'flex', gap: 'md', alignItems: 'center' })
-
-const linkStyles = css({
-  fontSize: 'sm',
-  color: 'primary.500',
-  fontWeight: '500',
-  width: 'calc(token(sizes.xl) - token(spacing.md) * 2)',
-  p: 'xs',
-  display: 'flex',
-  gap: 'sm',
-  alignItems: 'center',
-  minHeight: '29px',
-
-  _focus: {
-    outline: 'none',
-    bg: 'primary.100',
-  },
-
-  _hover: {
-    outline: 'none',
-    bg: 'primary.100',
-  },
-})
 
 export default function Nav(): JSX.Element {
   const { user, userData } = useAuth()
@@ -130,166 +109,74 @@ export default function Nav(): JSX.Element {
                   },
 
                   '--dropdown-height':
-                    'calc(token(spacing.md) * 2 + 29px * 9 + 9px * 4)',
+                    'calc(token(spacing.md) * 2 + 32px * 9 + 9px * 4)',
                 })}
                 align="end"
                 sideOffset={10}
               >
                 <nav>
-                  <DropdownMenu.Item
-                    asChild
-                    className={css({
-                      display: 'block',
-                      whiteSpace: 'nowrap',
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-
-                      fontSize: 'sm',
-                      color: 'primary.500',
-                      fontWeight: '500',
-                      width: 'calc(token(sizes.xl) - token(spacing.md) * 2)',
-                      p: 'xs',
-                      alignItems: 'center',
-                      minHeight: '29px',
-
-                      _focus: {
-                        outline: 'none',
-                        bg: 'primary.100',
-                      },
-
-                      _hover: {
-                        outline: 'none',
-                        bg: 'primary.100',
-                      },
-                    })}
-                  >
-                    <Link
-                      focus="disabled"
-                      hover="disabled"
-                      href={`/profile/${userData?.username ?? ''}`}
-                      weight="normal"
-                    >
-                      {userData?.username} - {user.email} asdf asdf asdf asdf
-                    </Link>
-                  </DropdownMenu.Item>
+                  <DropdownLink
+                    name={`${userData?.username ?? ''} - ${user.email ?? ''}`}
+                    href={`/profile/${userData?.username ?? ''}`}
+                  />
 
                   <Separator size="sm" className={css({ my: 'xs' })} />
 
-                  <DropdownMenu.Item asChild className={linkStyles}>
-                    <Link
-                      focus="disabled"
-                      hover="disabled"
-                      href="/universities"
-                      weight="normal"
-                    >
-                      <HiOutlineLibrary size={16} />
-                      Universidades
-                    </Link>
-                  </DropdownMenu.Item>
+                  <DropdownLink
+                    Icon={HiOutlineLibrary}
+                    name="Universidades"
+                    href="/universities"
+                  />
 
                   <Separator size="sm" className={css({ my: 'xs' })} />
 
-                  <DropdownMenu.Item asChild className={linkStyles}>
-                    <Link
-                      focus="disabled"
-                      hover="disabled"
-                      weight="normal"
-                      href="/account/profile"
-                    >
-                      <AiOutlineUser size={16} />
-                      Perfil
-                    </Link>
-                  </DropdownMenu.Item>
+                  <DropdownLink
+                    Icon={AiOutlineUser}
+                    name="Perfil"
+                    href="/account/profile"
+                  />
 
-                  <DropdownMenu.Item asChild className={linkStyles}>
-                    <Link
-                      focus="disabled"
-                      hover="disabled"
-                      weight="normal"
-                      href="/account/security"
-                    >
-                      <MdLockOutline size={16} />
-                      Seguridad
-                    </Link>
-                  </DropdownMenu.Item>
+                  <DropdownLink
+                    Icon={MdLockOutline}
+                    name="Seguridad"
+                    href="/account/security"
+                  />
 
-                  <DropdownMenu.Item asChild className={linkStyles}>
-                    <Link
-                      focus="disabled"
-                      hover="disabled"
-                      weight="normal"
-                      href="/account/payment"
-                    >
-                      <IoCardOutline size={16} />
-                      Pagos
-                    </Link>
-                  </DropdownMenu.Item>
+                  <DropdownLink
+                    Icon={IoCardOutline}
+                    name="Pagos"
+                    href="/account/payment"
+                  />
 
                   <Separator size="sm" className={css({ my: 'xs' })} />
 
-                  <DropdownMenu.Item asChild className={linkStyles}>
-                    <Link
-                      focus="disabled"
-                      hover="disabled"
-                      weight="normal"
-                      href="/account/balance"
-                    >
-                      <IoWalletOutline size={16} />
-                      Balance
-                    </Link>
-                  </DropdownMenu.Item>
+                  <DropdownLink
+                    Icon={IoWalletOutline}
+                    name="Balance"
+                    href="/account/balance"
+                  />
 
-                  <DropdownMenu.Item asChild className={linkStyles}>
-                    <Link
-                      focus="disabled"
-                      hover="disabled"
-                      weight="normal"
-                      href="/account/purchased"
-                    >
-                      <AiOutlineShoppingCart size={16} />
-                      Comprado
-                    </Link>
-                  </DropdownMenu.Item>
+                  <DropdownLink
+                    Icon={AiOutlineShoppingCart}
+                    name="Comprado"
+                    href="/account/purchased"
+                  />
 
-                  <DropdownMenu.Item asChild className={linkStyles}>
-                    <Link
-                      focus="disabled"
-                      hover="disabled"
-                      weight="normal"
-                      href="/account/uploaded"
-                    >
-                      <AiOutlineCloudUpload size={16} />
-                      Subido
-                    </Link>
-                  </DropdownMenu.Item>
+                  <DropdownLink
+                    Icon={AiOutlineCloudUpload}
+                    name="Subido"
+                    href="/account/uploaded"
+                  />
 
                   <Separator size="sm" className={css({ my: 'xs' })} />
 
-                  <DropdownMenu.Item
-                    asChild
-                    className={css({
-                      display: 'flex',
-                      width: 'calc(token(sizes.xl) - token(spacing.md) * 2)',
-                      p: 'xs',
-
-                      _focus: {
-                        bg: 'red.100',
-                        outline: 'none',
-                      },
-                    })}
-                  >
-                    <LinkButton
-                      visual="warning"
-                      size="sm"
-                      hover="disabled"
-                      focus="disabled"
-                      onClick={async () => {
-                        await user.signOut()
-                      }}
-                    >
-                      Cerrar sesión
-                    </LinkButton>
-                  </DropdownMenu.Item>
+                  <DropdownLink
+                    Icon={MdOutlineLogout}
+                    name="Cerrar sesión"
+                    onClick={async () => {
+                      await user.signOut()
+                    }}
+                  />
                 </nav>
               </DropdownMenu.Content>
             </DropdownMenu.Root>
