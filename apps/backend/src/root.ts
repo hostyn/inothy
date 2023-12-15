de93deae1013df60fc0f1077c69b2f6400329e7e
Hostyn <ruben@martinezhostyn.com>
2023-12-15 12:16:39 +0100

    feat(backend): :fire: Remove reviews endpoint

--- apps/backend/src/root.ts
+++ apps/backend/src/root.ts
@@ -3,14 +3,12 @@ import { authRouter } from './routes/auth'
 import { createTRPCRouter } from './trpc'
 import { documentRouter } from './routes/document'
 import { userRouter } from './routes/user'
-import { reviewsRouter } from './routes/reviews'
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   universities: universitiesRouter,
   document: documentRouter,
   user: userRouter,
-  reviews: reviewsRouter,
 })
 
 // export type definition of API
