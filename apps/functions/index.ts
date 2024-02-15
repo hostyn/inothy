43d8d44e78b81d25400e6ccf4a4e018c84fffed6
Hostyn <ruben@martinezhostyn.com>
2024-02-15 11:37:23 +0100

    feat(functions): :sparkles: Add KYC webhooks

--- apps/functions/index.ts
+++ apps/functions/index.ts
@@ -1,6 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { PrismaClient } from '@prisma/client'
 import * as functions from 'firebase-functions'
+import MangoPay from 'mangopay2-nodejs-sdk'
+
+const mangopay = new MangoPay({
+  clientId: process.env.MANGOPAY_CLIENT_ID as string,
+  clientApiKey: process.env.MANGOPAY_API_KEY as string,
+  baseUrl: process.env.MANGOPAY_ENDPOINT,
+})
 
 const client = new PrismaClient({
   log: ['info', 'query', 'warn'],
@@ -41,4 +48,49 @@ export const onRegister = functions
         username,
       },
     })
+  })
+
+export const kycWebhook = functions
+  // req.query =
+  // {
+  //   RessourceId: mangopayKycDocumentId,
+  //   EventType: KYC_SUCCEEDED | KYC_FAILED | KYC_OUTDATED,
+  //   Date: date in seconds -> new Date(date * 1000)
+  // }
+  .region('europe-west1')
+  .https.onRequest(async (req, res) => {
+    try {
+      const kycDocument = req.query.RessourceId
+
+      if (typeof kycDocument !== 'string') {
+        res.status(400).send('RessourceId_required')
+        return
+      }
+
+      // const eventType = req.query.EventType // KYC_SUCCEEDED | KYC_FAILED | KYC_OUTDATED
+      // TODO: send transactional email to user
+
+      const mangopayKycDocument = await mangopay.KycDocuments.get(kycDocument)
+
+      const user = await mangopay.Users.get(mangopayKycDocument.UserId)
+
+      await client.mangopayUser.update({
+        where: {
+          mangopayId: user.Id,
+        },
+        data: {
+          kycLevel: user.KYCLevel,
+          kycId: mangopayKycDocument.Id,
+          kycStatus: mangopayKycDocument.Status,
+          kycRefusedReasonMessage: mangopayKycDocument.RefusedReasonMessage,
+          kycRefusedReasonType: mangopayKycDocument.RefusedReasonType,
+          userType: user.UserCategory,
+        },
+      })
+
+      res.send('done')
+    } catch (error) {
+      console.error(error)
+      res.status(500).send('internal_server_error')
+    }
   })
