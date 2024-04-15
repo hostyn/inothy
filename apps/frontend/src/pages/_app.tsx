b1c15fb81d89f01abf1c0bd6f745e60ec7683727
Hostyn <ruben@martinezhostyn.com>
2024-04-15 11:59:36 +0200

    fix(frontend): :ambulance: Fix metatags duplication

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -14,6 +14,50 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="title" content="Inothy - Prepárate para aprobar"></meta>
+        <meta
+          name="description"
+          content="Compra los mejores apuntes y gana dinero vendiendo los tuyos."
+        ></meta>
+
+        <meta
+          property="og:url"
+          content={process.env.NEXT_PUBLIC_FRONTEND_URL}
+        ></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta
+          property="og:title"
+          content="Inothy - Prepárate para aprobar"
+        ></meta>
+        <meta
+          property="og:description"
+          content="Compra los mejores apuntes y gana dinero vendiendo los tuyos."
+        ></meta>
+        <meta
+          key="og:image"
+          property="og:image"
+          content={`${process.env.NEXT_PUBLIC_FRONTEND_URL as string}/og.png`}
+        ></meta>
+
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:domain" content="vercel.com"></meta>
+        <meta
+          property="twitter:url"
+          content={process.env.NEXT_PUBLIC_FRONTEND_URL}
+        ></meta>
+        <meta
+          name="twitter:title"
+          content="Inothy - Prepárate para aprobar"
+        ></meta>
+        <meta
+          name="twitter:description"
+          content="Compra los mejores apuntes y gana dinero vendiendo los tuyos."
+        ></meta>
+        <meta
+          key="twitter:image"
+          name="twitter:image"
+          content={`${process.env.NEXT_PUBLIC_FRONTEND_URL as string}/og.png`}
+        ></meta>
       </Head>
       <div className={`${nunito.className} ${nunitoSans.variable}`}>
         <PHProvider>
